48268262d590805d71ee9caa36ed38005e8f4449
Rodrigue Nzomo Nzogang <nzomo.rodrigue@tmrcomputing.com>
2024-10-07 16:08:43 +0100

    Top 34

--- backend/routes/aideRoutes.js
+++ backend/routes/aideRoutes.js
@@ -1,33 +1,50 @@
 const express = require("express");
 const router = express.Router();
 const aideController = require("../controllers/aideController");
-const verifierToken = require("../middlewares/authMiddleware");
-const verifierRole = require("../middlewares/roleMiddleware");
 
-// Routes CRUD pour les aides avec protection JWT et gestion des rôles
-router.get(
-  "/",
-  verifierToken,
-  verifierRole(["president", "tresorier"]),
-  aideController.listerAides
-);
-router.post(
-  "/",
-  verifierToken,
-  verifierRole(["tresorier"]),
-  aideController.creerAide
-);
-router.put(
-  "/:id",
-  verifierToken,
-  verifierRole(["tresorier"]),
-  aideController.modifierAide
-);
-router.delete(
-  "/:id",
-  verifierToken,
-  verifierRole(["president"]),
-  aideController.supprimerAide
-);
+// Routes CRUD pour les aides
+router
+  .route("/")
+  .get(aideController.listerAides) // Assurez-vous que listerAides est une fonction, pas un objet
+  .post(aideController.creerAide);
+
+router
+  .route("/:id")
+  .put(aideController.modifierAide)
+  .delete(aideController.supprimerAide);
 
 module.exports = router;
+
+// const express = require("express");
+// const router = express.Router();
+// const aideController = require("../controllers/aideController");
+// const verifierToken = require("../middlewares/authMiddleware");
+// const verifierRole = require("../middlewares/roleMiddleware");
+
+// // Routes CRUD pour les aides avec protection JWT et gestion des rôles
+// router.get(
+//   "/",
+//   verifierToken,
+//   verifierRole(["president", "tresorier"]),
+//   aideController.listerAides
+// );
+// router.post(
+//   "/",
+//   verifierToken,
+//   verifierRole(["tresorier"]),
+//   aideController.creerAide
+// );
+// router.put(
+//   "/:id",
+//   verifierToken,
+//   verifierRole(["tresorier"]),
+//   aideController.modifierAide
+// );
+// router.delete(
+//   "/:id",
+//   verifierToken,
+//   verifierRole(["president"]),
+//   aideController.supprimerAide
+// );
+
+// module.exports = router;
